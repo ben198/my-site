962690c9b686f96da2c8588528a4c6d1adb87d0a
Ben Townsend <btownsend220@gmail.com>
2016-09-23 09:07:21 +0100

    text moved out of markup and into data json file

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import IconMail from '../icons/IconMail';
+import data from '../../data.json';
 
 export default class Greeting extends Component {
     render() {
         return (
             <div className="greeting">
                 <div>
-                    <h2 className="greeting-heading">Hello. My name is Ben.</h2>
-                    <p className="greeting-text">Thank you for stopping by. I'm a front end developer based in Bristol, England. On this page you can find examples of some of my recent projects. I am taking on freelance work right now, so if you would like to work with me please do get in touch.</p>
+                    <h2 className="greeting-heading">{data.text.greeting.heading}</h2>
+                    <p className="greeting-text">{data.text.greeting.body}</p>
                     <a className="greeting-icon" href="mailto:btownsend220@gmail.com">
                         <IconMail/>
                     </a>
