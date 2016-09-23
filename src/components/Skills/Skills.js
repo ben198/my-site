8ab40330f7a252033a6c3f2c596663e5c992b870
Ben Townsend <btownsend220@gmail.com>
2016-09-23 22:09:30 +0100

    titles added

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,25 +8,62 @@ import IconJavascript from '../icons/IconJavascript';
 import IconReact from '../icons/IconReact';
 import IconSass from '../icons/IconSass';
 import IconWebpack from '../icons/IconWebpack';
+import data from '../../data.json';
 
 export default class Skills extends Component {
     render() {
         return (
             <div className="skills col-xs-6">
                 <ul className="row">
-                    <li className="skill-icon col-xs-4"><IconHtml5/></li>
-                    <li className="skill-icon col-xs-4"><IconCss3/></li>
-                    <li className="skill-icon col-xs-4"><IconJavascript/></li>
+                    <li
+                        className="skill-icon col-xs-4"
+                        title={data.text.skills.html}>
+                        <IconHtml5/>
+                    </li>
+                    <li
+                        className="skill-icon col-xs-4"
+                        title={data.text.skills.css}>
+                        <IconCss3/>
+                    </li>
+                    <li
+                        className="skill-icon col-xs-4"
+                        title={data.text.skills.js}>
+                        <IconJavascript/>
+                    </li>
                 </ul>
                 <ul className="row">
-                    <li className="skill-icon col-xs-4"><IconSass/></li>
-                    <li className="skill-icon col-xs-4"><IconAngular/></li>
-                    <li className="skill-icon col-xs-4"><IconGulp/></li>
+                    <li
+                        className="skill-icon col-xs-4"
+                        title={data.text.skills.sass}>
+                        <IconSass/>
+                    </li>
+                    <li
+                        className="skill-icon col-xs-4"
+                        title={data.text.skills.angular}>
+                        <IconAngular/>
+                    </li>
+                    <li
+                        className="skill-icon col-xs-4"
+                        title={data.text.skills.gulp}>
+                        <IconGulp/>
+                    </li>
                 </ul>
                 <ul className="row">
-                    <li className="skill-icon col-xs-4"><IconReact/></li>
-                    <li className="skill-icon col-xs-4"><IconWebpack/></li>
-                    <li className="skill-icon col-xs-4"><IconBootstrap/></li>
+                    <li
+                        className="skill-icon col-xs-4"
+                        title={data.text.skills.react}>
+                        <IconReact/>
+                    </li>
+                    <li
+                        className="skill-icon col-xs-4"
+                        title={data.text.skills.webpack}>
+                        <IconWebpack/>
+                    </li>
+                    <li
+                        className="skill-icon col-xs-4"
+                        title={data.text.skills.bootstrap}>
+                        <IconBootstrap/>
+                    </li>
                 </ul>
             </div>
         );
