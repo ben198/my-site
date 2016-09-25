861b859b6f1d571cc2c5bd700f9b9be3002ad73c
Ben Townsend <btownsend220@gmail.com>
2016-09-25 13:10:00 +0100

    component now depends on props for data. media query max-width changed. margin-bottom added

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,17 +1,36 @@
-import React, { Component } from 'react';
-import projectSrc from '../../public/images/tweets.jpg';
+import React, { Component, PropTypes } from 'react';
 
-export default class Project extends Component {
+class Project extends Component {
+
+    navigate() {
+        return location.href = this.props.link;
+    }
+
     render() {
         return (
             <div className="project">
-                <img className="project-img" src={projectSrc}/>
+                <img className="project-img" src={this.props.image}/>
                 <div className="project-info">
-                    <h3>React Google Map Tweets</h3>
-                    <p>By leveraging the features of libraries such as <a href="https://github.com/istarkov/google-map-react">google-map-react</a> and <a href="https://github.com/ttezel/twit">twit</a> I was able to create an app which appends tweet data, as a React component, to a Google Map. A Node server receives tweets from the Twitter streaming API, which are then piped to the client using <a href="https://github.com/socketio/socket.io">socket.io</a>. Still a work in progress but the app's most basic feature, getting data and displaying it, is working.</p>
-                    <button className="btn btn-primary" type="button" onclick="location.href='http://bentownsend.name/work/react-twitter/'">Have a look</button>
+                    <h3>{this.props.title}</h3>
+                    <p dangerouslySetInnerHTML={{__html: this.props.text}}></p>
+                    <button
+                        className="btn btn-primary"
+                        type="button"
+                        onClick={this.navigate.bind(this)}>
+                        Have a look
+                    </button>
                 </div>
             </div>
         );
     }
+
 }
+
+Project.propTypes = {
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired
+}
+
+export default Project;
