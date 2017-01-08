b77b39b32df9ba45d1900a7de8845f267558adb3
Ben Townsend <btownsend220@gmail.com>
2017-01-08 22:28:40 +0000

    Use Express to render app on the server and return as a string

---
+++ src/server/server.js
@@ -0,0 +1,33 @@
+import path from 'path';
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from './generated/app';
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+const port = 3000;
+const markup = renderToString(<App />);
+
+app.get('/', (req, res) => {
+    const page = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title>Ben Townsend</title>
+        </head>
+        <body>
+        <div id="container">${markup}</div>
+        <script src="/bundle.js"></script>
+        </body>
+        </html>
+    `;
+    res.send(page);
+});
+
+app.listen(port, () => {
+    console.log(`Node server is listening on port ${port}.`);
+});
