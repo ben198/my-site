fbd4ad8f882e0436de706a0dc309e56ca3c17f38
Ben Townsend <btownsend220@gmail.com>
2017-01-08 22:26:05 +0000

    Export an array containing two config objects. One for client and other for server rendering

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,71 @@
 const path = require('path');
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
-const buildPath = path.resolve(__dirname, 'dist');
-
-const settings = {
-    development: {
-        publicPath: '/assets/',
-        jsQueryPresets: ['react', 'es2015', 'react-hmre']
+const clientDir = path.resolve(__dirname, 'src/client');
+const serverDir = path.resolve(__dirname, 'src/server/generated');
+const distDir = path.resolve(__dirname, 'dist');
+const nodeModulesDir = path.resolve(__dirname, 'node_modules');
+const loaders = [
+    {
+        test: /\js?$/,
+        exclude: [nodeModulesDir],
+        loader: 'babel',
+        query: {
+            presets: ['react', 'es2015']
+        }
     },
-    production: {
-        publicPath: '/',
-        jsQueryPresets: ['react', 'es2015']
-    }
-};
-
-const isProduction = process.env.NODE_ENV === 'production';
-console.log(isProduction)
-const settingsToUse = isProduction ? settings.production : settings.development;
-
-module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: buildPath,
-        publicPath: settingsToUse.publicPath,
-        filename: 'bundle.js'
+    {
+        test: /\.css$/,
+        loader: 'style!css'
     },
-    module: {
-        loaders: [
-            {
-                test: /\js?$/,
-                exclude: [nodeModulesPath],
-                loader: 'babel',
-                query: {
-                    presets: settingsToUse.jsQueryPresets
-                }
-            },
-            {
-                test: /\.css$/,
-                loader: 'style!css'
-            },
-            {
-                test: /\.scss$/,
-                loaders: ['style', 'css', 'sass']
-            },
-            {
-                test: /\.json$/,
-                loader: 'json'
-            },
-            {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url?limit=10000&mimetype=application/font-woff"
-            },
-            {
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file"
-            },
-            {
-                test: /\.(png|jpg)$/,
-                loader: 'url?limit=8192'
-            }
-        ]
+    {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+    },
+    {
+        test: /\.json$/,
+        loader: 'json'
+    },
+    {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+    },
+    {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file"
+    },
+    {
+        test: /\.(png|jpg)$/,
+        loader: 'url?limit=8192'
     }
-}
+];
+
+module.exports = [
+    {
+        name: 'client',
+        target: 'web',
+        context: clientDir,
+        entry: './main.js',
+        output: {
+            path: distDir,
+            filename: 'bundle.js'
+        },
+        module: {
+            loaders: loaders
+        }
+    },
+    {
+        name: 'server',
+        target: 'node',
+        context: clientDir,
+        entry: {
+            app: './components/App.js'
+        },
+        output: {
+            path: serverDir,
+            filename: '[name].js',
+            libraryTarget: 'commonjs2'
+        },
+        module: {
+            loaders: loaders
+        }
+    }
+];
